0b2eb044a8ffe17263dfc7272c0ead293cad4086
pasipasipada <irfanoputra27@gmail.com>
2025-05-29 16:48:26 +0700

    feat: add ternary operator

--- src/to-do-list/ToDo.jsx
+++ src/to-do-list/ToDo.jsx
@@ -3,13 +3,7 @@ import React from "react";
 export default function ToDo({ text, isCompleted, isDeleted = false }) {
   if (isDeleted) {
     return null;
-  } else if (isCompleted) {
-    return (
-      <li>
-        <del>{text}</del>
-      </li>
-    );
   } else {
-    return <li>{text}</li>;
+    return <li>{isCompleted ? <del>{text}</del> : text}</li>;
   }
 }
