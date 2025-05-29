a8a2e87e9741be42428a310286a11484c735169d
pasipasipada <irfanoputra27@gmail.com>
2025-05-29 17:02:56 +0700

    feat: add collection of component

--- src/to-do-list/ToDoList.jsx
+++ src/to-do-list/ToDoList.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import ToDo from "./ToDo";
 
 export default function ToDoList() {
+  const data = [
+    { id: 1, text: "Learn HTML", isCompleted: true },
+    { id: 2, text: "Learn CSS", isCompleted: true },
+    { id: 3, text: "Learn JavaScript", isCompleted: true },
+    { id: 4, text: "Learn React", isCompleted: true },
+    { id: 5, text: "Learn Redux", isCompleted: false },
+    { id: 6, text: "Learn React Router", isCompleted: false },
+    { id: 7, text: "Learn React Hooks", isCompleted: false },
+    { id: 8, text: "Learn React Context", isCompleted: false },
+    { id: 9, text: "Learn React Testing Library", isCompleted: false },
+    { id: 10, text: "Learn React Native", isCompleted: false },
+  ];
+
   return (
     <ul>
-      <ToDo text="Learn HTML" isCompleted={true} isDeleted />
-      <ToDo text="Learn CSS" isCompleted={true} isDeleted />
-      <ToDo text="Learn JavaScript" isCompleted={true} />
-      <ToDo text="Learn React" isCompleted={true} />
-      <ToDo text="Learn Redux" isCompleted={false} />
-      <ToDo text="Learn React Router" isCompleted={false} />
-      <ToDo text="Learn React Hooks" isCompleted={false} />
-      <ToDo text="Learn React Context" isCompleted={false} />
-      <ToDo text="Learn React Testing Library" isCompleted={false} />
-      <ToDo text="Learn React Native" isCompleted={false} />
+      {data.map((todo) => (
+        <ToDo key={todo.id} {...todo} />
+      ))}
     </ul>
   );
 }
