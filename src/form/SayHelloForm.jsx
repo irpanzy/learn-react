47e4d0210d4cea39df679dbfb0c0674b755f992c
pasipasipada <irfanoputra27@gmail.com>
2025-05-30 19:24:15 +0700

    feat: add side effect

---
+++ src/form/SayHelloForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function SayHelloForm() {
+  function handleClick(e) {
+    e.preventDefault();
+    const name = document.getElementById("input_name").value;
+    document.getElementById("text_hello").innerText = `Hello ${name}`;
+  }
+  return (
+    <div>
+      <form>
+        <input id="input_name" type="text" />
+        <button onClick={handleClick}>Say Hello</button>
+      </form>
+      <h1 id="text_hello">Hello World</h1>
+    </div>
+  );
+}
