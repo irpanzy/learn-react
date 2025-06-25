08f0857d1c2795d20228eaf740058bb1bd45879e
pasipasipada <irfanoputra27@gmail.com>
2025-06-25 23:32:42 +0700

    feat: add custom hook

---
+++ src/online/Online.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import useOnline from "./OnlineHook";
+
+export default function Online() {
+  const isOnline = useOnline();
+
+  return <h1>{isOnline ? "You are online" : "You are offline"}</h1>;
+}
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
         timer: "timer.html",
         guest_book: "guest-book.html",
         product: "product.html",
+        online: "online.html",
       },
     },
   },
